3c54a841b92df47b51f468c41188cc03b3d677d5
Stéfano Girardelli <girardellisb@gmail.com>
2018-09-19 17:40:22 -0300

    Now you can check which numbers appeared more on all games we have in Brazil

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # winTheLottery
 Python script to read the most common APIs from Brazil's lotteries and return insights to make your bet.
+
+To run this code go to:
+https://confiraloterias.com.br/api/megasena/
+
+Get the Token code and change it on 'winTheLottery.py' on the 'token' var and rebuild it.
+
+Obs.: You need to do it everytime the API expire and you have problems with the request.
--- winTheLottery.py
+++ winTheLottery.py
@@ -2,18 +2,66 @@
 import urllib2, json
 import collections, numpy
 
-dezenas = []
+token = '3YdNWdm1mmk1oxE'
+total = []
 
-opener = urllib2.build_opener()
-opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-response = opener.open('http://confiraloterias.com.br/api0/json.php?loteria=megasena&lista=true&concurso=1&token=t6gmJaewcjkI7xf')
-data = json.loads(response.read())
+def duplasena():
+  return getResults('duplasena')
 
-for pair in data:
-  for value in pair.get("concurso").get("dezenas"):
-    dezenas.append(value)
+def lotofacil():
+  return getResults('lotofacil')
 
-ocurrencies = collections.Counter(dezenas)
-items = collections.Counter(str(item) for item in ocurrencies)
+def lotomania():
+  return getResults('lotomania')
 
-print ocurrencies
+def megasena():
+  return getResults('megasena')
+
+def quina():
+  return getResults('quina')
+
+def readAPI(category):
+  opener = urllib2.build_opener()
+  opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+  response = opener.open('http://confiraloterias.com.br/api0/json.php?loteria='+category+'&lista=true&concurso=1&token='+token)
+  return json.loads(response.read().decode('utf_8', 'ignore'))
+
+def getResults(category):
+  results = []
+  for pair in readAPI(category):
+    if category != 'duplasena':
+      for value in pair.get("concurso").get("dezenas"):
+        results.append(value)
+        total.append(value)
+    else:
+      for value in pair.get("concurso").get("dezenas_1"):
+        results.append(value)
+        total.append(value)
+      for value in pair.get("concurso").get("dezenas_2"):
+        results.append(value)
+        total.append(value)
+  return results
+
+print 'Duplasena:'
+for value in collections.Counter(duplasena()).most_common():
+  print value[0]+' appeared '+str(value[1])+' times'
+
+print '\nLotofacil:'
+for value in collections.Counter(lotofacil()).most_common():
+  print value[0]+' appeared '+str(value[1])+' times'
+
+print '\nLotomania:'
+for value in collections.Counter(lotomania()).most_common():
+  print value[0]+' appeared '+str(value[1])+' times'
+
+print '\nMegasena:'
+for value in collections.Counter(megasena()).most_common():
+  print value[0]+' appeared '+str(value[1])+' times'
+
+print '\nQuina:'
+for value in collections.Counter(quina()).most_common():
+  print value[0]+' appeared '+str(value[1])+' times'
+
+print '\n----\nEverything:'
+for value in collections.Counter(total).most_common():
+  print value[0]+' appeared '+str(value[1])+' times'
