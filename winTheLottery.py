bc5ab5be1e44fa78ca78d4fba6789aa6af23f910
Stéfano Girardelli <girardellisb@gmail.com>
2018-10-03 15:51:25 -0300

    Adding some examples of using the script

--- winTheLottery.py
+++ winTheLottery.py
@@ -2,10 +2,9 @@ from requestAPI import lotteryAPI
 
 # https://confiraloterias.com.br/api/megasena/
 goldenPot = lotteryAPI(False, 'FQrVkXLqxm13Wcj')
+
 goldenPot.megasena()
 
 for i in xrange(2):
   print goldenPot.makeABet(6, 60)
-
-
 
